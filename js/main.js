c0241014a548db12d8622cb72334ea859294e8bd
test <test@D-113063105.local>
2019-10-03 16:36:15 +0530

    nav bar styling active element

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Javascript Code goes here
+//jquery code to style nav bar cative element
 $(document).on('click','nav a',function(){
     $(this).addClass("select").siblings().removeClass("select");
 });

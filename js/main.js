6a8b06190793c2cfa2f894e261eb58012eb98633
test <test@D-113063105.local>
2019-10-04 14:38:48 +0530

    changing classes in html and css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Javascript Code goes here
 //jquery code to style nav bar cative element
 $(document).on('click','nav a',function(){
-    $(this).addClass("select").siblings().removeClass("select");
+    $(this).addClass("mk_select").siblings().removeClass("mk_select");
 });

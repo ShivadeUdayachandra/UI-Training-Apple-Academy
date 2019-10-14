a77cb546d071eadf21d1713582d76ca26d9b2676
AD20050770 <mounika.pulijala@wipro.com>
2019-10-14 15:10:22 +0530

    css changed and html files added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 // Javascript Code goes here
+//jquery code to style nav bar cative element
+$(document).on('click','nav a',function(){
+    $(this).addClass("mk_select").siblings().removeClass("mk_select");
+});

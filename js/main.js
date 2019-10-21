9bcda6d95bf4b7da499149ca2220eed778f81793
test <test@Jhon.local>
2019-10-21 16:50:29 +0530

    added login page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 // Javascript Code goes here
 //jquery code to style nav bar cative element
-$(document).on('click','nav a',function(){
+$(document).on('click', 'nav a', function () {
     $(this).addClass("mk_select").siblings().removeClass("mk_select");
 });
+
+function dr_check() {
+    name = document.getElementById("name").value;
+    pass = document.getElementById("pass").value;
+    if (name == "ramesh" && pass == "12345") {
+        return true;
+    }
+    else {
+        document.getElementById("error").innerHTML = "* Enter correct credentials......!"
+        return false;
+    }
+}

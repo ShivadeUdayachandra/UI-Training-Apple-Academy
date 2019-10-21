8bf564de40c1658b1838e29814a76dd5063df437
na20052196@wipro.com <test@D-156026998.local>
2019-10-21 17:09:24 +0530

    login page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 // Javascript Code goes here
+
+
+function bf_validate() {
+    var username = document.getElementById("bf_username").value;
+    var password = document.getElementById("bf_password").value;
+    if (username == "basil" && password == "123456") {
+        alert("Login successfully");
+        window.open("index_basil.html", "_self");
+    }
+    else {
+
+        alert("Login failed-please enter correct username and password");
+    }
+
+}
+
+
 //jquery code to style nav bar cative element
-$(document).on('click','nav a',function(){
+$(document).on('click', 'nav a', function () {
     $(this).addClass("mk_select").siblings().removeClass("mk_select");
 });

be4e6ed615e14f62a4355f15dc48b704d83c154f
test <test@D-113063105.local>
2019-10-21 17:07:06 +0530

    Adding the login page and its styling

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@
 $(document).on('click','nav a',function(){
     $(this).addClass("mk_select").siblings().removeClass("mk_select");
 });
+//login styling
+function mk_validate(){
+    var un= document.getElementById("mk_un").value;
+    var pa=document.getElementById("mk_pa").value;
+   
+    if(un=="manasa" && pa=="apple@123"){
+        return true;
+    }
+    else{
+        alert("enter valid details");
+        return false;
+    }
+ }
